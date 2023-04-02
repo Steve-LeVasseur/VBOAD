7cc15a4b63d206c69cfc11704a59250527827dbb
rafeldta <rafeldta@mail.uc.edu>
2023-04-01 21:03:38 -0400

    use opencv to read camera and identify with yolov8

--- run_video.py
+++ run_video.py
@@ -5,12 +5,12 @@ execution_path = os.getcwd()
 
 video_detector = CustomVideoObjectDetection()
 video_detector.setModelTypeAsYOLOv3()
-video_detector.setModelPath("/home/link232323/model/yolov3_model_mAP-0.89317_epoch-63.pt")
+video_detector.setModelPath("/home/link232323/model/models/yolov3_model_mAP-0.90437_epoch-97.pt")
 video_detector.setJsonPath("/home/link232323/model/json/model_yolov3_detection_config.json")
 video_detector.loadModel()
 
-video_detector.detectObjectsFromVideo(input_file_path="sidewalk.mp4",
-                                        output_file_path=os.path.join(execution_path, "sidewalk-detected"),
+video_detector.detectObjectsFromVideo(input_file_path="20230326_125808.mp4",
+                                        output_file_path=os.path.join(execution_path, "campus_detected"),
                                         frames_per_second=30,
                                         minimum_percentage_probability=40,
                                         log_progress=True)
